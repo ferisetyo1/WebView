a99adfa2ee0fcea17e799ae0e390d49541f61651
ferisetyo <android_dev_03@interactive.co.id>
2021-01-31 14:24:35 +0700

    Initial commit

---
+++ app/src/main/java/feri/com/webview/MainActivity.kt
@@ -0,0 +1,65 @@
+package feri.com.webview
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import im.delight.android.webview.AdvancedWebView
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() , AdvancedWebView.Listener{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        mWebView.setListener(this, this);
+        mWebView.setMixedContentAllowed(false);
+        mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @SuppressLint("NewApi")
+    override fun onResume() {
+        super.onResume()
+        mWebView.onResume()
+        // ...
+    }
+
+    @SuppressLint("NewApi")
+    override fun onPause() {
+        mWebView.onPause()
+        // ...
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        mWebView.onDestroy()
+        // ...
+        super.onDestroy()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        mWebView.onActivityResult(requestCode, resultCode, intent)
+        // ...
+    }
+
+    override fun onBackPressed() {
+        if (!mWebView.onBackPressed()) {
+            return
+        }
+        // ...
+        super.onBackPressed()
+    }
+
+    override fun onPageStarted(url: String?, favicon: Bitmap?) {}
+
+    override fun onPageFinished(url: String?) {}
+
+    override fun onPageError(errorCode: Int, description: String?, failingUrl: String?) {}
+
+    override fun onDownloadRequested(url: String?, suggestedFilename: String?, mimeType: String?, contentLength: Long, contentDisposition: String?, userAgent: String?) {}
+
+    override fun onExternalPageRequest(url: String?) {}
+}
